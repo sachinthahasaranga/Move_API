cdb6a4ccc47594458692dc7692feb4f114f7ebef
Sachintha Hasaranga <99173290+sachinthahasaranga@users.noreply.github.com>
2024-02-23 14:42:20 +0530

    create move class

--- movies/src/main/java/dev/farhan/movies/Movie.java
+++ movies/src/main/java/dev/farhan/movies/Movie.java
@@ -1,4 +1,28 @@
 package dev.farhan.movies;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collation = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Movie {
+
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private  String title;
+    private String releseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> generes;
+    private List<String> backdrops;
 }
